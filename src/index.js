3452ba77f41ce53de32e2c90594c5a747b2a73f5
nikmarsya <nikma944@gmail.com>
2022-07-18 19:28:21 +0800

    change back to browserrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import AnimatedRoute from './AnimatedRoute';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={window.location.pathname || ''}>
+    <Router basename={window.location.pathname || ''}>
         <AnimatedRoute />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
 
