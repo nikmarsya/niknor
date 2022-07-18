36e7036c82beaadcf9344a1b990d1356bde4e95d
nikmarsya <nikma944@gmail.com>
2022-07-18 15:57:35 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AnimatedRoute from './AnimatedRoute';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={window.location.pathname || ''}>
         <AnimatedRoute />
     </BrowserRouter>
   </React.StrictMode>
